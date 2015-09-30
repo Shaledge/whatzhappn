643258c37c947e88aade7873db9bef1fdf8ba100
Blissgig <blissgig@gmail.com>
2015-09-30 13:57:29 -0400

    Weather close to completed.

--- WhatzHappn/Default.aspx
+++ WhatzHappn/Default.aspx
@@ -5,13 +5,29 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title>What'z Happn</title>
+    <link href='https://fonts.googleapis.com/css?family=Piedra|Rye|Sarina|Jacques+Francois+Shadow' rel='stylesheet' type='text/css' />
+    <link href="../css/WhatzHappn Style.css" rel="stylesheet" type="text/css" />
 </head>
-<body>
-    <form id="Search" runat="server">
-    <div>
-    <textarea name="SearchBox" rows="1" cols="50"/><br />
-        <button name="SearchButton" />
+<body runat="server" id="WHBody">
+
+    <div class="header">
+        <div class="content">
+            <h1>What'z Happn</h1>
+
+            <div class="text">
+                <p>See whatz happenin in your city! </p>
+                <p>Its easy, just click the search button and we will use your current location to pull in the latest tweets, weather and news stories closest to you.</p>
+            </div>
+
+            <div id="button">
+                <a href="#" class="btn">Search</a>
+            </div>
+
+            <div class="map">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.617356072694!2d-73.9878530849208!3d40.74844454332262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9b3117469%3A0xd134e199a405a163!2sEmpire+State+Building!5e0!3m2!1sen!2sus!4v1443542824793" width="500" height="450" /> 
+            </div>
+        </div>
     </div>
-    </form>
+
 </body>
 </html>
--- WhatzHappn/Default.aspx.cs
+++ WhatzHappn/Default.aspx.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace WhatzHappn
 {
@@ -12,9 +15,12 @@ namespace WhatzHappn
         {
             try
             {
-                if (IsValidIP(GetIPAddress()) == true)
+                string sIPAddress = GetIPAddress();
+                string sZIPCode = "10001";
+
+                if (IsValidIP(sIPAddress) == true)
                 {
-                    //TODO: get info  
+                    GetWeather(sZIPCode);
                 }
                 else
                 {
@@ -25,6 +31,36 @@ namespace WhatzHappn
             {
                 logException(ex);
             }
+        }
+
+        private string GetIPAddress()
+        {
+            string sIPAddress = "";
+
+            try
+            {
+                System.Web.HttpContext context = System.Web.HttpContext.Current;
+                string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+                if (!string.IsNullOrEmpty(ipAddress))
+                {
+                    string[] addresses = ipAddress.Split(',');
+                    if (addresses.Length != 0)
+                    {
+                        sIPAddress = addresses[0];
+                    }
+                }
+                else
+                {
+                    sIPAddress = context.Request.ServerVariables["REMOTE_ADDR"];
+                }
+            }
+            catch (Exception ex)
+            {
+                logException(ex);
+            }
+
+            return sIPAddress;
         }
 
         private bool IsValidIP(string ipAddress)
@@ -63,26 +99,153 @@ namespace WhatzHappn
                 logException(ex);
             }
 
-            return bReturn;
+            return true; // bReturn; //TEMP TODO: remove for production
         }
 
-        private string GetIPAddress()
+        private void GetWeather(string ZIPCode)
         {
-            System.Web.HttpContext context = System.Web.HttpContext.Current;
-            string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
-
-            if (!string.IsNullOrEmpty(ipAddress))
+            try
             {
-                string[] addresses = ipAddress.Split(',');
-                if (addresses.Length != 0)
+                string sTitle = "";
+                XPathDocument xDoc = new XPathDocument("http://xml.weather.yahoo.com/forecastrss?p=" + ZIPCode);
+                XPathNavigator xNavigator;
+                XmlNamespaceManager xNameSpace;
+                XPathNodeIterator xNodes;
+                XPathNavigator xNode;
+                
+                Image WeatherIcon = new Image();
+                WeatherIcon.CssClass = "ButtonIcon";
+                WeatherIcon.ImageUrl = "images/" + "weather" + ".png";
+                
+                HtmlGenericControl SectionDIV = new HtmlGenericControl("div");
+                SectionDIV.Attributes["class"] = "header";
+                SectionDIV.ID = "Weather";
+                //SectionDIV.di
+                HtmlGenericControl ContentDIV = new HtmlGenericControl("div");
+                SectionDIV.Attributes["class"] = "content";
+                SectionDIV.ID = "WeatherContent";
+
+                SectionDIV.Controls.Add(ContentDIV);
+
+
+                SectionDIV.Style.Add("display", "block");
+
+                xNavigator = xDoc.CreateNavigator();
+
+                xNameSpace = new XmlNamespaceManager(xNavigator.NameTable);
+                xNameSpace.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+                xNodes = xNavigator.Select("/rss/channel/item/title", xNameSpace);
+
+                while (xNodes.MoveNext())
                 {
-                    return addresses[0];
+                    xNode = xNodes.Current;
+                    //sTitle = xNode.InnerXml.ToString();
+
+                    AddContentTile(SectionDIV, WeatherIcon, "", xNode.InnerXml.ToString());
+                }
+
+                //TEMP
+                this.WHBody.Controls.Add(SectionDIV);
+
+
+
+
+
+
+                xNodes = xNavigator.Select("/rss/channel/item/yweather:condition", xNameSpace);
+
+                //Basic: Temp and Condition
+                while (xNodes.MoveNext())
+                {
+                    xNode = xNodes.Current;
+                    sTitle = xNode.GetAttribute("temp", xNameSpace.DefaultNamespace) + "f";
+
+                    sTitle = xNode.GetAttribute("text", xNameSpace.DefaultNamespace);
+                }
+
+
+                //Sunrise and Sunset
+                xNodes = xNavigator.Select("/rss/channel/yweather:astronomy", xNameSpace);
+                while(xNodes.MoveNext())
+                {
+                    xNode = xNodes.Current;
+                    sTitle = "Sunrise: " + xNode.GetAttribute("sunrise", xNameSpace.DefaultNamespace);
+
+                    sTitle = "Sunset: " + xNode.GetAttribute("sunset", xNameSpace.DefaultNamespace);
+                }
+
+                //extended forcast
+                xNodes = xNavigator.Select("/rss/channel/item/yweather:forecast", xNameSpace);
+                while (xNodes.MoveNext())
+                {
+                    xNode = xNodes.Current;
+                    sTitle = xNode.GetAttribute("day", xNameSpace.DefaultNamespace) + ": " + xNode.GetAttribute("text", xNameSpace.DefaultNamespace);
+
                 }
             }
-
-            return context.Request.ServerVariables["REMOTE_ADDR"];
+            catch (Exception ex)
+            {
+                logException(ex);
+            }
         }
 
+        private void AddContentTile(HtmlGenericControl ParentDIV, Image Icon, string Title, string Body)
+        {
+            try
+            {
+                HtmlGenericControl WHTileDIV = new HtmlGenericControl("div");
+                WHTileDIV.Attributes["class"] = "whTile";
+                WHTileDIV.ID = "ContentTile" + NewGuid();
+
+                HtmlGenericControl Border3DIV = new HtmlGenericControl("div");
+                Border3DIV.Attributes["class"] = "border3";
+                Border3DIV.ID = "Border3" + NewGuid();
+
+                WHTileDIV.Controls.Add(Border3DIV);
+
+                HtmlGenericControl Border2DIV = new HtmlGenericControl("div");
+                Border2DIV.Attributes["class"] = "border3";
+                Border2DIV.ID = "Border3" + NewGuid();
+
+                Border3DIV.Controls.Add(Border2DIV);
+
+                if (Icon != null)
+                {
+                    HtmlGenericControl IconParagraph = new HtmlGenericControl("p");
+                    IconParagraph.Attributes["class"] = "monopolyLogo";
+                    IconParagraph.ID = "IconParagraph" + NewGuid();
+
+                    IconParagraph.Controls.Add(Icon);
+                }
+                else
+                {
+                    HtmlGenericControl TitleParagraph = new HtmlGenericControl("p");
+                    TitleParagraph.Attributes["class"] = "center";
+                    TitleParagraph.ID = "IconParagraph" + NewGuid();
+                    TitleParagraph.InnerText = Title;
+
+                    Border2DIV.Controls.Add(TitleParagraph);
+                }
+
+                HtmlGenericControl BodyParagraph = new HtmlGenericControl("p");
+                BodyParagraph.Attributes["class"] = "center";
+                BodyParagraph.ID = "BodyParagraph" + NewGuid();
+                BodyParagraph.InnerText = Body;
+
+                WHTileDIV.Controls.Add(BodyParagraph);
+
+                ParentDIV.Controls.Add(WHTileDIV);
+            }
+            catch (Exception ex)
+            {
+                logException(ex);
+            }
+        }
+
+        private string NewGuid()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
         private void logException(Exception ex)
         {
             Console.WriteLine(ex.Message);
